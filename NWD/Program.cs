8b2f264453a564de80aad2c7504bac74dcf5a9b1
cmorawski06 <127949584+cmorawski06@users.noreply.github.com>
2023-03-26 22:02:21 +0200

    Add files via upload

---
+++ NWD/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NWD
+{
+    class NWD
+    {
+        /* *******************************************************************
+         * nazwa funkcji:	NWD
+         * argumenty:		a - integer
+         *                  b - integer
+         * typ zwracany:	Zwracane jest zmodyfikowane a (integer).
+         * informacje:		Program prosi z poziomu konsoli o wprowadzenie dwóch liczb całkowitych (a oraz b). Następnie podaje ich największy wspólny dzielnik.
+         * autor:		    Cyprian Morawski nr 16
+         * ******************************************************************* */
+        static int nwd(int a, int b)
+        {
+            while (a != b)
+            {
+
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+        }
+        public static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Największy wspólny dzielnik liczb " + a + " i " + b + " to " + nwd(a, b));
+            Console.ReadKey();
+        }
+
+    }
+
+}
